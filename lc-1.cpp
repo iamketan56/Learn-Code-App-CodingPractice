33bcf9ac2c8dc3a8fbc24e27f3434885e83cfd5a
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-15 08:31:25 +0530

    1st day

---
+++ lc-1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+    for(i=1;i<=10;i++)
+    {
+        cout<<(n*i)<<endl;
+    }
+
+}
