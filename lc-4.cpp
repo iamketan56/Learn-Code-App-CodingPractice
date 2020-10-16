517b236368d6920de4d1803aa035c6d907ce459c
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-16 08:29:17 +0530

    Code

---
+++ lc-4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int total=800;
+    int i=8;
+    int j=0;
+    while(i<=total)
+    {
+        j++;
+        i=i+8;
+    }
+    cout<<""<<j;
+}
