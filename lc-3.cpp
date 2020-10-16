2ef826bf2d6921009345eb6d10d8d6f2ae6bc520
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-16 08:28:51 +0530

    Code

---
+++ lc-3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   // cout<<"we have two cups  of 500ml and 300ml"<<endl;
+    int i=500;
+    int j=300;
+    int res=i-j;
+    cout<<"here the water is "<<res<<"which we want to make our dish";
+}
