612a13d84d92e4b920d47fae4342b793f5bd9caa
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-17 10:30:26 +0530

    Code

---
+++ lc-7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long int x=1200000;
+    long int y;
+    for(int i=1;i<=365;i++)
+    {
+      y=x*i;
+    }
+   return y;
+}//incorrect
