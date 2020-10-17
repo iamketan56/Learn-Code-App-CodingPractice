04c0a7a50ab92e24e15b0f81419f885394033871
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2020-10-17 10:31:21 +0530

    Code

---
+++ lc-8.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int nh=8;int np=10; int nc=9;int ng=8;int nm=7;
+    float avg=(float)(nh+np+nc+ng+nm)/5;
+    cout<<""<<avg<<endl;
+    return avg;
+}
